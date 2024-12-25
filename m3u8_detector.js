638bca3683af73be04cb6464674102ece9703eaa
qbasekostas <qbasekostas@yahoo.com>
2024-12-25 23:06:19 +0200

    Update m3u8_detector.js

--- m3u8_detector.js
+++ m3u8_detector.js
@@ -92,12 +92,8 @@ async function commitAndPushChanges() {
     execSync('git config --global user.email "your-email@example.com"');
     execSync('git config --global user.name "your-username"');
 
-    // Add remote if it doesn't exist
-    try {
-        execSync(`git remote add origin https://${GITHUB_TOKEN}@github.com/${REPO_OWNER}/${REPO_NAME}.git`);
-    } catch (error) {
-        console.log('Remote origin already exists. Skipping adding remote.');
-    }
+    // Update the remote URL if it already exists
+    execSync(`git remote set-url origin https://${GITHUB_TOKEN}@github.com/${REPO_OWNER}/${REPO_NAME}.git`);
 
     execSync('git add .');
     execSync('git commit -m "Add M3U playlist"');
