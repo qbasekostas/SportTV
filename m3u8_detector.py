e66ab624ad8ef0d8a7493d5de953680e508728f6
qbasekostas <qbasekostas@yahoo.com>
2024-12-28 20:47:00 +0200

    Update m3u8_detector.py

--- m3u8_detector.py
+++ m3u8_detector.py
@@ -48,7 +48,7 @@ def find_m3u8_links(url):
     # Εξαγωγή M3U8 συνδέσμων και των Referer από τις αιτήσεις δικτύου
     m3u8_links = set()  # Χρησιμοποιούμε set για να αποθηκεύσουμε μοναδικούς συνδέσμους
     for request in driver.requests:
-        if request.response and '.m3u8' in request.url:
+        if request.response and request.response.headers.get('Content-Type') == 'application/vnd.apple.mpegurl':
             referer = request.headers.get('Referer', 'N/A')
             stream_name = request.url.split('/')[-2]  # Εξαγωγή του ονόματος του stream από το URL
             m3u8_links.add((stream_name, request.url, referer))
@@ -59,6 +59,7 @@ def find_m3u8_links(url):
             print(f"URL: {request.url}")
             print(f"Method: {request.method}")
             print(f"Status Code: {request.response.status_code if request.response else 'No response'}")
+            print(f"Content-Type: {request.response.headers.get('Content-Type') if request.response else 'No response'}")
 
     print(f"Found {len(m3u8_links)} unique M3U8 links.")
     return list(m3u8_links)
