ca97a1b6da06c0fb3840a16d7bce47d248c23b59
qbasekostas <qbasekostas@yahoo.com>
2025-01-05 16:17:20 +0200

    Update playwright-script.js

--- playwright-script.js
+++ playwright-script.js
@@ -4,7 +4,7 @@ const { getRandomUserAgent } = require('./useragent_generator');
 
 (async () => {
     const targetUrls = [
-        'https://foothubhd.org/cdn3/linka.php',
+       'https://foothubhd.org/cdn3/linka.php',
         'https://foothubhd.org/cdn3/linkb.php',
         'https://foothubhd.org/cdn3/linkc.php',
         'https://foothubhd.org/cdn3/linkd.php',
@@ -24,81 +24,73 @@ const { getRandomUserAgent } = require('./useragent_generator');
         browser = await chromium.launch({ headless: true, args: ['--no-sandbox'] });
 
         for (const targetUrl of targetUrls) {
-            const page = await browser.newPage();
+             const page = await browser.newPage();
 
-              //Try a different user agent
+             //Try a different user agent
              const randomUserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
-             await page.setExtraHTTPHeaders({
-               'User-Agent': randomUserAgent,
-                 'Accept': '*/*',
-                'Accept-Language': 'el-GR,el;q=0.8,en-US;q=0.5,en;q=0.3',
-                'Connection': 'keep-alive',
+           await page.setExtraHTTPHeaders({
+                'User-Agent': randomUserAgent,
+                 'Referer': 'https://foothubhd.org/',
+                 'Origin': 'https://foothubhd.org',
+               'Accept': '*/*',
+               'Accept-Language': 'el-GR,el;q=0.8,en-US;q=0.5,en;q=0.3',
+               'Connection': 'keep-alive',
+           });
+
+             page.on('pageerror', (err) => {
+              console.log("\x1b[31m Javascript error:\x1b[0m", err.message, err.stack, targetUrl)
             });
-
-
-           page.on('pageerror', (err) => {
-                  console.log("\x1b[31m Javascript error:\x1b[0m", err.message, err.stack, targetUrl)
-             });
-
 
             try {
                 console.log("\x1b[34mFetching page content:\x1b[0m", targetUrl);
-                 const start = Date.now();
-                const response = await page.goto(targetUrl, { waitUntil: 'domcontentloaded', timeout: 5000 });
-                  console.log("\x1b[33m Page loaded with status:\x1b[0m", response.status(), targetUrl, ` in ${Date.now() - start}ms`);
+                const start = Date.now();
+                const response = await page.goto(targetUrl, { waitUntil: 'domcontentloaded', timeout: 5000, javaScriptEnabled:false });
+                console.log("\x1b[33m Page loaded with status:\x1b[0m", response.status(), targetUrl, ` in ${Date.now() - start}ms`);
+                  const pageContent = await page.content();
 
-                 // Dynamically Get Referer
-                const referer = await page.evaluate(()=> document.referrer || 'https://foothubhd.org/');
-                 console.log("\x1b[32mReferer: \x1b[0m", referer);
-
-              page.on('response', async (response) => {
-                    const url = response.url();
-                    if (!response.ok()) {
-                        console.log("\x1b[33mResponse failed for url: \x1b[0m", url);
-                        return;
-                    }
-                    try{
-                        const content = await response.text();
-                         const genericMatch = content.match(/source:\s*window\.atob\('(.*?)'\)/);
-                           if (genericMatch && genericMatch[1]) {
-                             const decodedM3U8= Buffer.from(genericMatch[1], 'base64').toString('utf-8');
-                             const streamName = new URL(decodedM3U8).pathname.split('/').slice(-2, -1)[0];
-                            m3u8Links.add({ streamName, url: decodedM3U8, referer });
-                             console.log(`\x1b[32mFound .m3u8 URL in XHR response:\x1b[0m`,decodedM3U8);
-                           }
-                       }
-                       catch(e){
-                           console.log("\x1b[33mCould not get the M3U8 url from response body. \x1b[0m", url, e);
-                       }
-                  });
+                 const scriptMatch = pageContent.match(/source:\s*window\.atob\('(.*?)'\)/);
+                if(scriptMatch && scriptMatch[1]){
+                       const decodedM3U8= Buffer.from(scriptMatch[1], 'base64').toString('utf-8');
+                     const streamName = new URL(decodedM3U8).pathname.split('/').slice(-2, -1)[0];
+                      m3u8Links.add({ streamName, url: decodedM3U8, referer:  'https://foothubhd.org/' });
+                     console.log(`\x1b[32mFound .m3u8 URL by generic regex:\x1b[0m`,decodedM3U8);
+                }
+                else{
+                  console.log("\x1b[33m⚠️ Could not get the M3U8 url by generic regex.\x1b[0m",targetUrl);
+                   await page.screenshot({ path: `error_screenshot_${Date.now()}.png` });
+                }
                  await delay(5000); // Add delay between page loads.
+
+
             } catch (navigationError) {
                console.error("\x1b[31mError processing page:\x1b[0m", navigationError, targetUrl);
                  await page.screenshot({ path: `error_screenshot_${Date.now()}.png` });
           } finally {
-                await page.close();
-           }
-        }
+              await page.close();
+          }
+       }
 
 
          // Taξιvόμηση και αποθήκευση των URLs
         const parsedLinks = Array.from(m3u8Links).sort((a, b) => a.streamName.localeCompare(b.streamName));
-         let playlistContent = "#EXTM3U\n";
-       parsedLinks.forEach(entry => {
+        let playlistContent = "#EXTM3U\n";
+        parsedLinks.forEach(entry => {
            playlistContent += `#EXTINF:-1,${entry.streamName}\n#EXTVLCOPT:http-referrer=${entry.referer}\n${entry.url}\n`;
        });
         fs.writeFileSync('playlist.m3u8', playlistContent);
 
         if (parsedLinks.length) {
-            console.log(`\x1b[32m✅ Total .m3u8 URLs found: ${parsedLinks.length}\x1b[0m`);
+           console.log(`\x1b[32m✅ Total .m3u8 URLs found: ${parsedLinks.length}\x1b[0m`);
         } else {
            console.log("\x1b[33m⚠️ No .m3u8 URL found.\x1b[0m");
-         }
-   } catch (error) {
-      console.error("\x1b[31mAn unexpected error occurred:\x1b[0m", error);
+       }
+
+
+     } catch (error) {
+       console.error("\x1b[31mAn unexpected error occurred:\x1b[0m", error);
     } finally {
-       if (browser) {
+        if (browser) {
            await browser.close();
-        }
-   }
+       }
+    }
 })();
